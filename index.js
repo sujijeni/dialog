45455a541c1529aff189587c7dfcb576dc07e70e
sujijeni <40774934+sujijeni@users.noreply.github.com>
2018-07-03 02:14:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let request = require('request');
+
+let apiKey = 'be4f4efc8956564f9d185322c48231b6';
+let city = 'chennai';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
+  }
+});
